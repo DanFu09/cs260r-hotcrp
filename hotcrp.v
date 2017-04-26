337803c44d1a07ef2ffd73df7fbaf36ce8a64389
Richard Cho <rcho@college.harvard.edu>
2017-04-26 00:26:58 -0400

    fix spec of opt

--- hotcrp.v
+++ hotcrp.v
@@ -121,7 +121,7 @@ Module HotCRP.
     (* A simple optimization for simple_policy_map:
     Move the entire user query into SQL, and replace any instance of
     (Decision = x) with
-    (Team = u.team) || (Decision = x && !(Team = u.team)) *)
+    (Team = u.team && 0 = x) || (Decision = x && !(Team = u.team)) *)
     Fixpoint simple_optimization (uq:user_query) (u:user) :
       (user_query * sql_query) :=
       match u with
@@ -129,10 +129,10 @@ Module HotCRP.
         match uq with
         | Field_eq (Paper_decision x) =>
             (Sql_true,
-            Or (Field_eq (Paper_team team)) (And uq (Field_neq (Paper_team team))))
+            Or (And (Field_eq (Paper_team team)) (Field_eq (Paper_decision 0))) (And uq (Field_neq (Paper_team team))))
         | Field_neq (Paper_decision x) =>
             (Sql_true,
-            Or (Field_eq (Paper_team team)) (And uq (Field_neq (Paper_team team))))
+            Or (And (Field_eq (Paper_team team)) (Field_neq (Paper_decision 0))) (And uq (Field_neq (Paper_team team))))
         | And q1 q2 =>
             (Sql_true,
             And (snd (simple_optimization q1 u)) (snd (simple_optimization q2 u)))
